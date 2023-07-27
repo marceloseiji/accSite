aed1fc9b0d9f558746e459807813b3fc769760db
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-27 15:46:16 -0300

    feat: remove scss files, add styled components elements

---
+++ src/styles/styleReset.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const StyleReset = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+    overflow: auto;
+  }
+`
---
+++ src/styles/variables.ts
@@ -0,0 +1,3 @@
+export const sizes = {
+  mobile: '600px'
+}
