03eb270949c8597ce2c53e8fc8745a2bae7761c7
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-28 01:05:12 -0300

    chore: font adjustments

--- webpack/common.js
+++ webpack/common.js
@@ -52,6 +52,10 @@ module.exports = env => {
         {
           test: /\.(s(a|c)ss)$/i,
           use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource'
         }
       ]
     },
