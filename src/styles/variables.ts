947edb59c3ca0cbc86d11b6baeecc5c3dad38ca6
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-28 01:06:00 -0300

    feat: add wrapper

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,3 +1,15 @@
-export const sizes = {
-  mobile: '600px'
+export const screenSize = {
+  mobile: '600px',
+  tablet: '768px',
+  laptop: '1024px'
+}
+
+export const deviceSize = {
+  mobile: `(min-width: ${screenSize.mobile})`,
+  tablet: `(min-width: ${screenSize.tablet})`,
+  laptop: `(min-width: ${screenSize.laptop})`
+}
+
+export const textSize = {
+  small: '1rem'
 }
