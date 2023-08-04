fd6c06c1e16e7efae537c3b7b1c808b06bc1cde6
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-08-03 21:39:37 -0300

    feat: adding drag element

---
+++ src/components/dragIcon/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const IconWrapper = styled.div`
+  bottom: 12vh;
+  position: fixed;
+`
--- src/containers/main/index.tsx
+++ src/containers/main/index.tsx
@@ -1,12 +1,14 @@
 import Home from 'containers/main/sections/home/index'
 import BottomNav from 'containers/main/components/bottomNav'
 import { MainSection } from 'containers/main/style'
+import DragIcon from 'components/dragIcon'
 
 const Main = () => (
   <>
     <MainSection>
       <Home />
     </MainSection>
+    <DragIcon />
     <BottomNav />
   </>
 )
