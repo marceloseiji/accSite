d93225ea9089f5dbe2278b2b8f2adbdfeb2610c0
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-08-04 12:38:04 -0300

    feat: completes dargUp icon

--- src/components/dragIcon/style.ts
+++ src/components/dragIcon/style.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
-export const IconWrapper = styled.div`
+export const DragImgWrapper = styled.div`
   bottom: 12vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
   position: fixed;
 `
+
+export const DragArrowUpImg = styled.img`
+  margin-bottom: 4px;
+  position: absolute;
+`
+
+export const DragBackgroundImg = styled.img``
