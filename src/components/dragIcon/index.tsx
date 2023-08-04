89dc7714e8a7c2910722edc8d28a1fee8d2cde3b
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-08-04 11:20:34 -0300

    chore: svg import fix

--- src/components/dragIcon/index.tsx
+++ src/components/dragIcon/index.tsx
@@ -1,7 +1,6 @@
 import { IconWrapper } from './style'
 import getImageHash from 'utils/getImageHash'
 import DragUpArrows from 'assets/icons/DragUpArrows.svg'
-import Squirrel from 'assets/icons/kindpng_1131539.png'
 
 const DragIcon = () => {
   return (
