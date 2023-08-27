41c5d7c48a4b37b8e79fd953cdaaaeda5bd5cee6
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-08-27 20:12:33 -0300

    chore: update to hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import MainWrapper from 'components/wrapper'
 
@@ -8,10 +8,10 @@ const container = document.getElementById('root')
 if (container) {
   const root = createRoot(container)
   root.render(
-    <BrowserRouter>
+    <HashRouter>
       <MainWrapper>
         <App />
       </MainWrapper>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
