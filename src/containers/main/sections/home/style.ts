8f6cc2c4a7dcfd977f2005fa7fb51079ff386d94
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-08-27 14:01:43 -0300

    feat: styled components organization

--- src/containers/main/sections/home/style.ts
+++ src/containers/main/sections/home/style.ts
@@ -1,15 +1,6 @@
 import styled from 'styled-components'
-import imgUrl from 'assets/images/home-background.jpg'
 import getImageHash from 'utils/getImageHash'
 import { colors, textSize } from 'styles/variables'
-
-export const Section = styled.section`
-  background-image: url(${getImageHash(imgUrl)});
-  background-position: center;
-  background-size: cover;
-  width: 100%;
-  height: 100%;
-`
 
 export const TitleWrapper = styled.div`
   padding-top: 10vh;
