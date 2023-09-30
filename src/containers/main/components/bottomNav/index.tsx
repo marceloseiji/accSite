5536110910408aa3d81da3503d4fc39ef3c32655
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-09-30 01:19:38 -0300

    feat: wip starting out to keep menu items active

--- src/containers/main/components/bottomNav/index.tsx
+++ src/containers/main/components/bottomNav/index.tsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react'
 import { Link, List, Nav } from 'containers/main/components/bottomNav/style'
 
-const BottomNav = () => (
-  <Nav aria-label="Menu do site">
-    <List>
-      <Link href="#home-section">Home</Link>
-      <Link href="#psicanalise-section">Psicanálise</Link>
-      <Link href="#about-section">Sobre</Link>
-      <Link href="#contact-section">Contato</Link>
-    </List>
-  </Nav>
-)
+const BottomNav = () => {
+  const [active, setActive] = useState('') // WIP deixar os itens do menu ativos
+
+  useEffect(() => {
+    console.log()
+  })
+
+  return (
+    <Nav aria-label="Menu do site">
+      <List>
+        <Link href="#home-section">Home</Link>
+        <Link href="#psicanalise-section">Psicanálise</Link>
+        <Link href="#about-section">Sobre</Link>
+        <Link href="#contact-section">Contato</Link>
+      </List>
+    </Nav>
+  )
+}
 
 export default BottomNav
