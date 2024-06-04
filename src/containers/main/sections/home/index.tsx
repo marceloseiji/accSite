79137aa689c31cb4e65143558ac1cda1f62a7863
Marcelo Seiji <ikuta.marcelo@gmail.com>
2024-06-03 21:54:25 -0300

    feat: home and psicanalise wip

--- src/containers/main/sections/home/index.tsx
+++ src/containers/main/sections/home/index.tsx
@@ -4,18 +4,22 @@ import {
   Hr,
   TextWrapper,
   HomeWrapper,
-  HomeImage
+  HomeImage,
+  ImageWrapper
 } from './style'
 import { Section } from 'containers/main/style'
 import { SectionDivider } from 'components/sectionDivider/style'
 import imgUrl from 'assets/images/home-background.jpg'
 import getImageHash from 'utils/getImageHash'
+import ResponsiveWrapper from 'components/responsiveWrapper'
 
 const Home = () => {
   return (
-    <Section id="home-section">
+    <Section url="" id="home-section">
       <HomeWrapper src={getImageHash(imgUrl)}>
-        <HomeImage src={getImageHash(imgUrl)} width="45%" height="100%" />
+        <ImageWrapper>
+          <HomeImage src={getImageHash(imgUrl)} width="42%" height="60%" />
+        </ImageWrapper>
         <TextWrapper>
           <Title>AMANDA CALVETTI CORRÊA</Title>
           <Hr />
--- src/containers/main/sections/psicanalise/index.tsx
+++ src/containers/main/sections/psicanalise/index.tsx
@@ -1,29 +1,33 @@
 import { Section } from 'containers/main/style'
-import { TextWrapper, Title, Paragraph } from './style'
+import { TextWrapper, Title, Paragraph, PsicanaliseWrapper } from './style'
 import ResponsiveWrapper from 'components/responsiveWrapper'
-import imgUrl from 'assets/images/psicanalise-background.jpg'
+import imgUrl from 'assets/images/psicanalise-background-full.jpg'
 import { SectionDivider } from 'components/sectionDivider/style'
+import getImageHash from 'utils/getImageHash'
 
 const Psicanalise = () => {
   return (
     <Section url={imgUrl} id="psicanalise-section">
-      <ResponsiveWrapper>
-        <TextWrapper>
-          <Title>Como a psicanálise pode te ajudar?</Title>
-          <Paragraph>
-            A psicanálise é uma teoria e método terapêutico criado por Sigmund
-            Freud, focado na compreensão das influências inconscientes na mente
-            humana. Envolve a exploração de desejos, memórias reprimidas e
-            conflitos emocionais através da livre associação e interpretação,
-            visando trazer insight, alívio emocional e mudanças comportamentais.
-          </Paragraph>
-          <Paragraph>
-            Ela influenciou profundamente a psicologia e o entendimento da mente
-            humana.
-          </Paragraph>
-        </TextWrapper>
-        <SectionDivider color="#14181b" />
-      </ResponsiveWrapper>
+      <PsicanaliseWrapper src={getImageHash(imgUrl)}>
+        <ResponsiveWrapper>
+          <TextWrapper>
+            <Title>Como a psicanálise pode te ajudar?</Title>
+            <Paragraph>
+              A psicanálise é uma teoria e método terapêutico criado por Sigmund
+              Freud, focado na compreensão das influências inconscientes na
+              mente humana. Envolve a exploração de desejos, memórias reprimidas
+              e conflitos emocionais através da livre associação e
+              interpretação, visando trazer insight, alívio emocional e mudanças
+              comportamentais.
+            </Paragraph>
+            <Paragraph>
+              Ela influenciou profundamente a psicologia e o entendimento da
+              mente humana.
+            </Paragraph>
+          </TextWrapper>
+        </ResponsiveWrapper>
+      </PsicanaliseWrapper>
+      <SectionDivider color="#14181b" />
     </Section>
   )
 }
